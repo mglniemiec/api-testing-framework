1bde61a8c5688812f46c9455fb550697bc99049a
Magdalena Niemiec <magdalena@Magdalenas-MacBook-Pro.local>
2025-06-10 10:36:11 +0200

    Add 404 test for non_existent user

--- tests/test_user_detail_get.py
+++ tests/test_user_detail_get.py
@@ -6,3 +6,7 @@ def test_get_user_returns_200(base_url):
     assert response.status_code == 200
     validate_response(response.json(), "data/user_detail_schema.json")
 
+def test_get_user_not_found_returns_404(base_url):
+    response = requests.get(f"{base_url}/users/23")
+    assert response.status_code == 404
+    assert response.json() == {} 
