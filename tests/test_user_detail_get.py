cf8920932e5d0f76703bce776bbbb671ce144223
Magdalena Niemiec <magdalena@Magdalenas-MacBook-Pro.local>
2025-06-10 10:32:39 +0200

    add response and shema validation for user detail endpoint

---
+++ tests/test_user_detail_get.py
@@ -0,0 +1,8 @@
+import requests
+from utils.schema_validator import validate_response
+
+def test_get_user_returns_200(base_url):
+    response = requests.get(f"{base_url}/users/2")
+    assert response.status_code == 200
+    validate_response(response.json(), "data/user_detail_schema.json")
+
